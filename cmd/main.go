c592ac1b8d18b2f25f0cab2bd5596c7b407f1475
Anton <antonlashkov@sumoscheduler.com>
2023-08-03 22:59:31 +0300

    add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	eventsapi "github.com/salesforceanton/events-api"
 	"github.com/salesforceanton/events-api/config"
@@ -42,11 +45,35 @@ func main() {
 	services := service.NewService(repos, cfg)
 	handler := handler.NewHandler(services)
 
+	// Run server
 	server := new(eventsapi.Server)
-	if err := server.Run(cfg.Port, handler.InitRoutes()); err != nil {
+	go func() {
+		if err := server.Run(cfg.Port, handler.InitRoutes()); err != nil {
+			log.WithFields(log.Fields{
+				"problem": fmt.Sprintf("Error with server running/or server in closing: %s", err.Error()),
+			}).Error(err)
+			return
+		}
+	}()
+
+	// Gracefull shutdown
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGTERM, syscall.SIGINT)
+	<-exit
+
+	if err := server.Shutdown(context.Background()); err != nil {
 		log.WithFields(log.Fields{
-			"problem": fmt.Sprintf("Error with server running: %s", err.Error()),
+			"problem": fmt.Sprintf("Error when server was shutting down: %s", err.Error()),
 		}).Error(err)
 		return
 	}
+
+	if err := db.Close(); err != nil {
+		log.WithFields(log.Fields{
+			"problem": fmt.Sprintf("Error with closing database connection: %s", err.Error()),
+		}).Error(err)
+		return
+	}
+
+	log.Info("Server shutdown successfully")
 }
